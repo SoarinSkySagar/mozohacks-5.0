6f19ee65e7c210447af963d92f2155d05564269e
SoarinSkySagar <sagarrana.pvt@gmail.com>
2024-03-28 19:14:49 +0530

    changes

--- server/app.py
+++ server/app.py
@@ -9,12 +9,15 @@ def index():
     return render_template('index.html')
 
 @app.route('/input', methods=['POST', 'GET'])
-def input():
+def input_data():
     if request.method == 'POST':
         data = request.get_json()
-        if 'idea' in data and 'type' in data:
+
+        if 'idea' in data and 'type' in data:      
+
             project_type = data['type']
             project_idea = data['idea']
+
             dev_result = create_project(project_type, project_idea)
             print(dev_result)
             return jsonify({'project_type': project_type, 'project_idea': project_idea, 'response': dev_result})
