58fe01aedc6b6d2f679e3274077650d48402222a
SoarinSkySagar <sagarrana.pvt@gmail.com>
2024-03-28 13:27:49 +0530

    crew taking inputs from flask endpoint now

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from crew.main import create_project
 
 app = Flask(__name__)
 
@@ -6,6 +7,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/input', methods=['POST', 'GET'])
+def input():
+    if request.method == 'POST':
+        data = request.get_json()
+        if 'idea' in data and 'type' in data:
+            project_type = data['type']
+            project_idea = data['idea']
+            dev_result = create_project(project_type, project_idea)
+            print(dev_result)
+            return jsonify({'project_type': project_type, 'project_idea': project_idea})
+        else:
+            return jsonify({'error': 'Invalid data'})
+    else:
+        return render_template('input.html')    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- server/crew/main.py
+++ server/crew/main.py
@@ -4,62 +4,48 @@ load_dotenv()
 from langchain_google_genai import ChatGoogleGenerativeAI
 from crewai import Crew, Process
 
-from tasks import DevelopmentTasks
-from agents import DevelopmentAgents
+from .tasks import DevelopmentTasks
+from .agents import DevelopmentAgents
 
-# Set up the language model
-llm = ChatGoogleGenerativeAI(model="gemini-pro",
-                             verbose=True,
-                             temperature=0.5,
-                             google_api_key="AIzaSyAh-36X7c0aMzbxQB0B_CxPZ8UcctQqzHA")
+llm = ChatGoogleGenerativeAI(model="gemini-pro", verbose=True, temperature=0.5)
 
 tasks = DevelopmentTasks()
 agents = DevelopmentAgents()
 
-print("## Welcome to the Development Crew")
-print('-------------------------------')
-
-project_type = input("What type of project do you want to develop? (website, app, game): ")
-
-# Ask for project idea
-project_idea = input("Please describe your project idea: ")
-input_agent = agents.input_agent(project_idea)
-ui_ux_agent = agents.ui_ux_agent(project_idea)
 junior_scraper_agent = agents.junior_scraper_agent()
 senior_scraper_agent = agents.senior_scraper_agent()
 cto_agent = agents.cto_agent()
 ceo_agent = agents.ceo_agent()
 devrel_agent = agents.devrel_agent()
 
-# Create Tasks
-if project_type == "website":
-    development_task = tasks.website_development_task(cto_agent, project_idea)
-    development_agents = [ui_ux_agent, agents.frontend_junior_agent(), agents.frontend_senior_agent(),
-                          agents.lead_frontend_agent(), agents.backend_junior_agent(), agents.main_backend_agent(),
-                          agents.integration_dev_agent(), agents.tester_agent(), agents.senior_developer_agent()]
-elif project_type == "app":
-    development_task = tasks.app_development_task(cto_agent, project_idea)
-    development_agents = [agents.junior_fe_agent(), agents.senior_fe_agent(), agents.backend_dev_agent(), agents.app_tester_agent()]
-elif project_type == "game":
-    development_task = tasks.game_development_task(cto_agent, project_idea)
-    development_agents = [agents.junior_game_dev_agent(), agents.senior_game_dev_agent(), agents.game_tester_agent()]
-else:
-    print("Invalid project type. Please try again.")
-    exit()
+def create_project(project_type, project_idea):
 
-# Create Crew
-development_crew = Crew(
-    agents=development_agents + [input_agent, ui_ux_agent, junior_scraper_agent, senior_scraper_agent, cto_agent, ceo_agent, devrel_agent],
-    tasks=[development_task],
-    verbose=True,
-    process=Process.sequential
-)
+    input_agent = agents.input_agent(project_idea)
+    ui_ux_agent = agents.ui_ux_agent(project_idea)
 
-print(f"Input Agent: Assigning {project_type.capitalize()} Development Crew")
-development_result = development_crew.kickoff()
+    if project_type == "website":
+        development_task = tasks.website_development_task(cto_agent, project_idea)
+        development_agents = [ui_ux_agent, agents.frontend_junior_agent(), agents.frontend_senior_agent(),
+                            agents.lead_frontend_agent(), agents.backend_junior_agent(), agents.main_backend_agent(),
+                            agents.integration_dev_agent(), agents.tester_agent(), agents.senior_developer_agent()]
+    elif project_type == "app":
+        development_task = tasks.app_development_task(cto_agent, project_idea)
+        development_agents = [agents.junior_fe_agent(), agents.senior_fe_agent(), agents.backend_dev_agent(), agents.app_tester_agent()]
+    elif project_type == "game":
+        development_task = tasks.game_development_task(cto_agent, project_idea)
+        development_agents = [agents.junior_game_dev_agent(), agents.senior_game_dev_agent(), agents.game_tester_agent()]
+    else:
+        print("Invalid project type. Please try again.")
+        exit()
 
-# Print results
-print("\n\n################################################")
-print("## Development Result")
-print("################################################\n")
-print(development_result)
+    development_crew = Crew(
+        agents=development_agents + [input_agent, ui_ux_agent, junior_scraper_agent, senior_scraper_agent, cto_agent, ceo_agent, devrel_agent],
+        tasks=[development_task],
+        verbose=True,
+        process=Process.sequential,
+        
+    )
+
+    development_result = development_crew.kickoff()
+
+    return development_result
